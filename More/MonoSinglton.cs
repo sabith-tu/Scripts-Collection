9467d8691d32dc9df597b5c7fa354018a75bc7f5
Sabi <95301637+sabith-tu@users.noreply.github.com>
2024-11-30 00:21:54 +0530

    Create MonoSinglton.cs

---
+++ More/MonoSinglton.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class MonoSinglton<T> : MonoBehaviour
+    where T : MonoBehaviour
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+
+                if (_instance == null)
+                {
+                    GameObject singletonObject = new GameObject();
+                    _instance = singletonObject.AddComponent<T>();
+                    singletonObject.name = typeof(T).ToString() + " (Singleton)";
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
