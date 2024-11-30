bc241d7cf18c5fc165e3d9d8d4ab5092f0ae0fa6
Sabi <95301637+sabith-tu@users.noreply.github.com>
2024-12-01 00:23:11 +0530

    Update MonoSinglton.cs

--- More/MonoSinglton.cs
+++ More/MonoSinglton.cs
@@ -11,14 +11,9 @@ public class MonoSinglton<T> : MonoBehaviour
         {
             if (_instance == null)
             {
-                _instance = FindObjectOfType<T>();
-
-                if (_instance == null)
-                {
-                    GameObject singletonObject = new GameObject();
-                    _instance = singletonObject.AddComponent<T>();
-                    singletonObject.name = typeof(T).ToString() + " (Singleton)";
-                }
+                GameObject singletonObject = new GameObject();
+                _instance = singletonObject.AddComponent<T>();
+                singletonObject.name = typeof(T).ToString() + " (Singleton)";
             }
 
             return _instance;
